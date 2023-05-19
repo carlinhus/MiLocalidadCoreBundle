6273b0ba3a95756db81b95e1ce088b544ea6b042
Carlos Campo <carlinhus@carlinhus.com>
2023-05-19 18:23:57 +0200

    Zone added

---
+++ src/Entity/Zone.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MiLocalidad\CoreBundle\src\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use MiLocalidad\Repository\MiLocalidad\CoreBundle\Entity\ZoneRepository;
+
+#[ORM\Entity(repositoryClass: ZoneRepository::class)]
+class Zone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?int $reference = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getReference(): ?int
+    {
+        return $this->reference;
+    }
+
+    public function setReference(int $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+}
